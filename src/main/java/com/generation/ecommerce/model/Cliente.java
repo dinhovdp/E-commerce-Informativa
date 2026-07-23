7d43f02c5b36bc63a3abe99bfc31333bfc37f89b
Edson Silva <dinhovdp@gmail.com>
2026-07-23 00:15:46 -0300

    :sparkles: Class Cliente e Pedido Finalizado

--- src/main/java/com/generation/ecommerce/model/Cliente.java
+++ src/main/java/com/generation/ecommerce/model/Cliente.java
@@ -1,5 +1,74 @@
 package com.generation.ecommerce.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_cliente")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório!")
+    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
+    private String nome;
+
+    @NotBlank(message = "O e-mail é obrigatório!")
+    @Column(length = 255)
+    @Email(message = "Digite um e-mail válido!")
+    private String email;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("cliente")
+    private List<Pedido> pedidos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+  
 }
+
--- src/main/java/com/generation/ecommerce/model/Pedido.java
+++ src/main/java/com/generation/ecommerce/model/Pedido.java
@@ -1,5 +1,110 @@
 package com.generation.ecommerce.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_pedido")
 public class Pedido {
 
+	@Id // PRIMARY KEY
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // id AUTO_INCREMENT
+    private Long id;
+
+	@NotBlank(message = "O atributo produto é obrigatório!")
+	@Size(min = 2, max = 100, message = "O atributo produto deve ter no mínimo 2 e no máximo 100 caracteres")
+	@Column(length = 100)
+    private String produto;
+
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	@Size(min = 2, max = 1000, message = "O atributo descrição deve ter no mínimo 2 e no máximo 1000 caracteres")
+	@Column(length = 1000)
+    private String descricao;
+
+    @NotNull(message = "O valor é obrigatório!")
+	@DecimalMin(value = "0.01", message = "O valor deve ser maior que zero!")
+	@Digits(integer = 10, fraction = 2, message = "O valor deve ter no máximo 10 dígitos inteiros e 2 decimais")
+	@Column(nullable = false, precision = 12, scale = 2) //total 12 digitos, sendo 2 decimais.
+    private BigDecimal valor;
+
+    
+    //registra apenas a criacao do pedido e nao atualiza toda vez que altera
+    @CreationTimestamp
+    private LocalDate data;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    @JsonIgnoreProperties("pedidos")
+    private Cliente cliente;
+
+    public Pedido() {
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProduto() {
+		return produto;
+	}
+
+	public void setProduto(String produto) {
+		this.produto = produto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+  
 }
